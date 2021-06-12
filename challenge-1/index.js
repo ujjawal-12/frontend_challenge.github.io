4591367f86df05b661e30cb566a54d66d94c0b17
ujjawa-12 <67854090+Ujjawal-kp@users.noreply.github.com>
2021-06-12 16:17:49 +0530

    Adding javascript file

---
+++ challenge-1/index.js
@@ -0,0 +1,17 @@
+(() => {
+  const btn = document.getElementById("hamburger");
+  const ulist = document.getElementsByTagName("ul")[0];
+  const img=document.querySelector('button#hamburger img');
+  btn.addEventListener("click", action);
+  function action() {
+    if (ulist.classList.contains("nav-item"))
+    {
+      ulist.classList.toggle("nav-item");
+      img.src="./images/icon-hamburger.svg"
+    }
+    else{
+      ulist.classList.toggle("nav-item");
+      img.src="./images/icon-close.svg"
+    }
+  }
+})();
